9d881df999c41de018851cdd63f8bee073a5bf1c
shunsuke-kawata <kwt20238120@gmail.com>
2023-07-20 22:55:16 +0900

    create python ファイルを実行するcppスクリプトの作成

--- testpy_from_cpp.py
+++ testpy_from_cpp.py
@@ -1,1 +1,15 @@
-print("firebase通信を行う関数の実行")
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+def send_data_to_firebase():
+    # Firebaseのサービスアカウント情報
+    cred = credentials.Certificate('test-cpp-to-firebase-firebase-adminsdk-ruihs-55eb777c49.json')
+    app = firebase_admin.initialize_app(cred)
+    db = firestore.client()
+
+    doc_ref = db.collection("positions").document("test")
+    doc_ref.set({"x": 100, "y": 200, "z": 50})
+    print("added")
+
+send_data_to_firebase()
